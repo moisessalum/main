80e52901e0410f8c35f2add99df6722d92b31a38
Moises <33235386+moisessalum@users.noreply.github.com>
2018-07-23 11:40:59 -0500

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,4 +1,5 @@
 sudo apt-get install zsh
 sudo apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-sudo chsh -s /bin/zsh
+# First create a password for the user to then change to ZSH
+chsh -s `which zsh`
