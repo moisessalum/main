9ecf7412a8409bc0fd8e60ab7ee2ac3247f932eb
Moises <33235386+moisessalum@users.noreply.github.com>
2018-07-23 11:43:22 -0500

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,5 +1,7 @@
+# https://gist.github.com/tsabat/1498393
 sudo apt-get install zsh
 sudo apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 # First create a password for the user to then change to ZSH
+# https://aws.amazon.com/premiumsupport/knowledge-center/set-change-root-linux/
 chsh -s `which zsh`
