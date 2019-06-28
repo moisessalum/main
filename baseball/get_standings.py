290ea7c04f1aef6f8e3bb7ae97c1862c711b1c91
Moises Rodriguez Salum <mrodriguez@Adeccomx.mex>
2019-06-28 15:59:08 -0500

    Classification problem.

--- baseball/get_standings.py
+++ baseball/get_standings.py
@@ -11,9 +11,9 @@ for i in years:
     df_n = pd.read_csv('c:/users/mrodriguez/Desktop/GL{}.txt'.format(i), header=None)
     df_list.append(df_n)
 
-result = pd.concat(df_list)
+result = pd.concat(df_list, ignore_index=True)
 
 result.columns = columns
 
-df = df.append(result)
+df = df.append(result, ignore_index=True)
 df.to_csv('c:/Users/mrodriguez/Desktop/2000_2018.csv')
