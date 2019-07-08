650fef9d98702011a505617ba07bf5279fe851df
Moises Salum <rodriguez.fq@gmail.com>
2019-07-07 22:43:22 -0500

    Function to determine new record

--- baseball/get_standings.py
+++ baseball/get_standings.py
@@ -1,5 +1,34 @@
 import pandas as pd
 from datetime import datetime
+import numpy as np
+
+
+def get_new_record(winner=None, df=None, ix=None):
+    # Update team record
+    try:
+        winner = row['visiting_team']
+        all_visit_games_array = df_year[df_year['visiting_team']==winner].index.values
+        all_home_games_array = df_year[df_year['home_team']==winner].index.values
+        all_team_games = np.concatenate((all_visit_games_array, all_home_games_array), axis=None)
+        current_index = np.where(all_team_games==ix)[0][0]
+        if current_index == 0:
+            pass
+        elif current_index > 0:
+            previous_index = current_index - 1
+            previous_game = all_team_games[previous_index]
+            print("PG", previous_game)
+            previous_record = df[winner].iloc[previous_game]
+            print("Prev rec", previous_record)
+            next_index = current_index + 1
+            next_game = all_team_games[next_index]
+            # print("Next ix", next_index)
+            next_record = previous_record + 1
+            # print("Next rec", next_record)
+            df_year[winner].iloc[next_game] = next_record
+            print(df_year[winner].iloc[next_game])
+    except Exception as e:
+        print("Visiting score exception:", e)
+
 
 # Read data
 df = pd.read_csv('1991_2018.csv', index_col=0)
@@ -8,7 +37,7 @@ df = pd.read_csv('1991_2018.csv', index_col=0)
 df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
 df['year'] = pd.DatetimeIndex(df['date']).year
 df['month'] = pd.DatetimeIndex(df['date']).month
-df = df.sort_values(by='date')
+df = df.sort_index()
 
 # Get teams list and add to df
 teams = df['home_team'].unique()
@@ -22,15 +51,15 @@ unique_year = [1991]
 for year in unique_year:
     df_year = df[df['year'] == year]
     for ix, row in df_year.iterrows():
+        print(ix)
+        # print(row['visiting_team'])
+        # print(row['home_team'])
         if row['visiting_score'] > row['home_score']:
-            # df_year[row['visiting_team']] = df_year[row['visiting_team']] + 1
-            print(ix)
-            index_list = df_year.loc[df_year['visiting_team']==row['visiting_team']].index.values
-            print(index_list)
-            list_value = index_list.index(ix)
-            print(list_value)
-        else:
-            df_year[row['home_team']] = df_year[row['home_team']] + 1
+            winner = row['visiting_team']
+            get_new_record(winner=winner, df=df_year, ix=ix)
+        elif row['visiting_score'] < row['home_score']:
+            winner = row['home_team']
+            get_new_record(winner=winner, df=df_year, ix=ix)
     print('Done', year)
     year_list_df.append(df_year)
 
